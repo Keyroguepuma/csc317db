cb7a48dbd59fea4248e84df1080b4db373055eb3
Adrian Jimenez <ajimenez2266@gmail.com>
2023-07-12 21:24:33 -0700

    part 2 almost done

---
+++ application/public/js/index.js
@@ -0,0 +1,50 @@
+var container = document.getElementById('main-content');
+var url = "https://jsonplaceholder.typicode.com/albums/2/photos";
+
+function fadeout(ev){
+    const divElement = ev.currentTarget;
+    var counter = 1;
+    var timer = setInterval(function(){
+        counter -= 0.05;
+        if(counter < 0){
+            clearInterval(timer);
+            container.removeChild(divElement)
+        }
+        console.log(counter);
+        divElement.style.opacity = counter;
+    },10);
+
+}
+
+function buildCardDOMAPI(data){
+    var cardDiv = document.createElement('div');
+    cardDiv.setAttribute('class', "photo");
+    var imgTag=document.createElement('img');
+    imgTag.src = data.thumbnailUrl;
+    imgTag.alt = "something";
+    var titleTag = document.createElement('p');
+    titleTag.appendChild(document.createTextNode(data.title));
+    cardDiv.appendChild(imgTag);
+    cardDiv.appendChild(titleTag);
+    cardDiv.addEventListener('click', fadeout);
+    return cardDiv;
+}
+
+
+function build(data){
+    return  `<div class="photo"> 
+    <img src="${data.thumbnailUrl}" alt=""> 
+    <p>${data.title}</p>
+     </div>`;
+}
+
+fetch(url)
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data){
+        var cards = data.map(function(photo){
+            return buildCardDOMAPI(photo);
+        });
+        container.append(...cards);
+    })
