08aa62f68793dbba7eca449165a4a5795354184d
Adrian Jimenez <ajimenez2266@gmail.com>
2023-07-28 13:39:58 -0700

    milestone 2

--- application/routes/index.js
+++ application/routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Adrian Jimenez" });
 });
+router.get("/login" , function(req,res,next){
+  res.render('login')
+})
+router.get("/postvideo" , function(req,res,next){
+  res.render('postvideo')
+})
+router.get("/registration" , function(req,res,next){
+  res.render('registration')
+})
+router.get("/viewpost" , function(req,res,next){
+  res.render('viewpost')
+})
 
 module.exports = router;
