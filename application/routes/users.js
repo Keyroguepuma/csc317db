e90d9fe3dcd61570074067f6b06ae1dd589316f6
Adrian Jimenez <ajimenez2266@gmail.com>
2023-08-05 22:03:05 -0700

    milestone 3

--- application/routes/users.js
+++ application/routes/users.js
@@ -1,8 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../config/database');
+
 //localhost:300/users/registration
-router.post('/registration', function(req,res,next){
-  console.log(req.body);
-  res.end();
+router.post('/registration', async function(req,res,next){
+  var {username,email,password} = req.body;
+  //server side validation
+  try{
+    var [results, _] = await db.query(`select id from users where username = ?`, [username])
+    if(results && results.length > 0){
+      return res.redirect('/registration')
+    } 
+    var [results, _] = await db.query(`select id from users where email = ?`, [email])
+     if(results && results.length > 0){
+      return res.redirect('/registration')
+    }
+
+    // insert into db
+    var [insertResult, _] = await db.execute(`INSERT INTO users (username, email, password) VALUE (?,?,?)`, [username,email,password])
+
+
+    //respond
+    if(insertResult && insertResult.affectedRows == 1){
+      return res.redirect('/login')
+
+    }else{
+      return res.redirect('/register');
+
+    }
+
+  }catch(err){
+    next(err);
+  }
+
+
 })
 module.exports = router;
